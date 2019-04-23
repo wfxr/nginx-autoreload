14c1b638b0fb5366708fd7124e6d73b8afa373b4
Wenxuan Zhang <wenxuangm@gmail.com>
2019-04-23 17:37:30 +0800

    Improve reloading logic.

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,7 @@
 FROM nginx:1.15-alpine
 
+RUN apk add --no-cache bash
+
 ADD autoreload.sh /etc/nginx/autoreload.sh
 
 CMD ["sh", "-c", "/etc/nginx/autoreload.sh & nginx -g 'daemon off;'"]
--- autoreload.sh
+++ autoreload.sh
@@ -1,17 +1,19 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 ################################################################################
 #    Author: Wenxuan Zhang                                                     #
 #     Email: wenxuangm@gmail.com                                               #
 #   Created: 2019-03-19 16:41                                                  #
 ################################################################################
+conf=(
+/etc/nginx/nginx.conf
+/etc/nginx/conf.d/*
+)
 
-conf=/etc/nginx/nginx.conf
-
-sum=$(cksum "$conf")
+sum=$(cksum "${conf[@]}")
 while true; do
-    newsum=$(cksum "$conf")
+    newsum=$(cksum "${conf[@]}")
     if [ "$newsum" != "$sum" ]; then
-        echo "$(date '+%Y/%m/%d %T') [$0] nginx.conf update detected."
+        echo "$(date '+%Y/%m/%d %T') [$0] nginx.conf update detected." >> /etc/nginx/autoload.log
         sum=$newsum
         nginx -t && nginx -s reload
     fi
